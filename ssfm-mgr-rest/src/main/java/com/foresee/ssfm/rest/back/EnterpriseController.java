3dcfc3802a68ba4f3a3b1989bea3b3e2c2f020ee
wuqiujie <wuqiujie@foresee.com.cn>
2019-03-18 11:31:51 +0800

    ##SOCIALSEC-902 test

--- ssfm-mgr-rest/src/main/java/com/foresee/ssfm/rest/back/EnterpriseController.java
+++ ssfm-mgr-rest/src/main/java/com/foresee/ssfm/rest/back/EnterpriseController.java
@@ -44,6 +44,7 @@ public class EnterpriseController {
         System.out.println(enterpriseDTO);
         System.out.println(enterpriseDTO);
         System.out.println(enterpriseDTO);
+        System.out.println(enterpriseDTO);
         return Response.successData(enterpriseDTO);
     }
 }
